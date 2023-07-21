2e144e1659147070475b2a5035f6fff75a5f05cd
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2023-07-21 17:12:15 +0700

    setup project

---
+++ prisma/migrations/20230721095608_create_database_classification_and_images_table/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "images" (
+    "id" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+
+    CONSTRAINT "images_pkey" PRIMARY KEY ("id")
+);
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import app from './app/app.js';
+
+console.log(process.env.APP_PORT);
